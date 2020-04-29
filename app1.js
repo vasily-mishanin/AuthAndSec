545ba5ffc1f330408e0b05360d515eed7a766d83
Vasily Mishanin <vasilymishanin@gmail.com>
2020-04-29 17:05:06 +0300

    open passwords and usernames

--- app1.js
+++ app1.js
@@ -4,14 +4,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 
 const app = express();
-console.log(process.env.SECRET_KEY); // log "SECRET_KEY" from ".env" file
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+//console.log(process.env.SECRET_KEY); // log "SECRET_KEY" from ".env" file
+
+app.use(express.static("public")); // using "public" folder for statics
+app.set("view engine", "ejs"); // Embedded Javascript
+app.use(bodyParser.urlencoded({ extended: true })); // to take parsed values fron the pages
 
 //connect to MongoDB
 const dbName = "userDB";
@@ -19,24 +19,21 @@ mongoose.connect(`mongodb://localhost:27017/${dbName}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.set("useCreateIndex", true);
 
 //creating new Schema
 const userSchema = new mongoose.Schema({
   name: String,
   password: String,
+  googleId: String,
+  facebookId: String,
+  secret: String,
 });
 
-///////////////////////////////////////////////Encryption////////////////////////////////////////////////////////////////////
-//encrypt dosc with 'userSchema' when User.save() and decrypt when User.find()
-// process.env.SECRET_KEY -> grabs SECRET_KEY from .env file
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET_KEY,
-  encryptedFields: ["password"],
-});
-
-//creating new collection
+//creating new collection ("users") as a model
 const User = new mongoose.model("User", userSchema);
 
+////////////////////////////////// ROUTES ////////////////////////
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -49,8 +46,15 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.get("/secrets", (req, res) => {
+  res.render("secrets");
+});
+
+app.get("/submit", (req, res) => {
+  res.render("submit");
+});
+
 app.post("/register", (req, res) => {
-  console.log(req.body);
   const newUser = new User({
     name: req.body.username,
     password: req.body.password,
@@ -64,11 +68,11 @@ app.post("/register", (req, res) => {
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const requestedPassword = req.body.password;
-
   User.findOne({ name: username }, (err, foundUser) => {
     if (err) {
       console.log(err);
     } else {
+      //  compare stored at "userDB"  "password" with "requestedPassword"
       if (foundUser) {
         foundUser.password === requestedPassword
           ? res.render("secrets")
@@ -77,10 +81,11 @@ app.post("/login", (req, res) => {
     }
   });
 });
-//////////////////////////////////////////////
+
+/////////////////Listener/////////////////////////////
 localPort = 5000;
 app.listen(localPort, (err) => {
   !err
-    ? console.log(`Server is running in port ${localPort}`)
+    ? console.log(`Server is running at port ${localPort}`)
     : console.log(err);
 });
